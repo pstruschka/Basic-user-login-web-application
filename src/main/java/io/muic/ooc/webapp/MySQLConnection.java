c16723f6382b3da55cf720cdad1327b9696c852b
Peter Son Struschka <me@peter-struschka.com>
2017-02-13 22:50:52 +0700

    added mysqlconnection class

---
+++ src/main/java/io/muic/ooc/webapp/MySQLConnection.java
@@ -0,0 +1,14 @@
+package io.muic.ooc.webapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySQLConnection {
+
+    public MySQLConnection(){
+
+    }
+}
